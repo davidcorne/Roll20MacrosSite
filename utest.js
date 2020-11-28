39a05466d4f2fe69c7dd116a594b96b94282fbdb
davidcorne <davidcorne@gmail.com>
2020-11-28 12:22:13 +0000

    Add unit test framework

---
+++ utest.js
@@ -0,0 +1,11 @@
+'use strict'
+/* global describe, it */
+const chai = require('chai')
+chai.use(require('chai-string'))
+const assert = chai.assert
+
+describe('Macro generation', function() {
+    it('Test', function() {
+        assert.strictEqual(0, 0)
+    })
+})
